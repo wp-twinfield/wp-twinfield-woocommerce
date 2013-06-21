f9316e98e83c05a12c30ac946574d353f86cc6b8
Leon Rowland <leon@rowland.nl>
2013-06-21 14:54:25 +0200

    invoice metabox contents

---
+++ views/woocommerce_invoice_meta_box_view.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Metabox view for the WooCommerce Order Metabox.
+ * 
+ * Will submit the order via ajax to the FormBuilder methods.
+ * 
+ * @author Leon Rowland <leon@rowland.nl>
+ */
+
+?>
+<div class="WoocommerceTwinfieldSyncMessageHolder"></div>
+<input class="WoocommerceTwinfieldSyncButton button button-primary" type="submit" name="woocommerce_twinfield_sync" value="<?php _e( 'Sync', 'woocommerce-twinfield' ); ?>"/>

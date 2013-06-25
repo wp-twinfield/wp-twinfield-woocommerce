beb3039a4678f0cac060c7605af114fb2c0d3cc0
Leon Rowland <leon@rowland.nl>
2013-06-25 09:18:17 +0200

    gets the invoice number and custom ID. requires making the WC order, and then passing that into the woocommerce invoice class.
    save method now passes customer id to sync in sync class

--- lib/class-woocommerce-invoice-meta-box.php
+++ lib/class-woocommerce-invoice-meta-box.php
@@ -2,7 +2,7 @@
 
 class Woocommerce_Invoice_Meta_Box {
 	
-	private $woocommerce_invoice_sync;
+	public $woocommerce_invoice_sync;
 	
 	public function __construct() {
 		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
@@ -28,10 +28,22 @@ class Woocommerce_Invoice_Meta_Box {
 		
 		// Get the admin script
 		wp_enqueue_script( 'woocommerce_twinfield_sync_js', plugins_url( '/admin/woocommerce_twinfield_sync.js', $woocommerce_twinfield->plugin_file() ), array( 'jquery' ) );
+		wp_localize_script( 'woocommerce_twinfield_sync_js', 'Woocommerce_Twinfield_Vars', array(
+			'spinner' => admin_url( 'images/wpspin_light.gif' )
+		) );
+		
+		$wc_order = new WC_Order();
+		$wc_order->populate( $post );
+		
+		$invoice = new \Woocommerce_Invoice( $wc_order );
+		$invoice_number = $invoice->check_for_twinfield_invoice_number();
+		$customer_id = $invoice->check_for_twinfield_customer_id();
 		
 		$view = new \ZFramework\Base\View( $woocommerce_twinfield->plugin_folder() . '/views' );
 		$view
 			->setView( 'woocommerce_invoice_meta_box_view' )
+			->setVariable( 'customer_id', $customer_id )
+			->setVariable( 'invoice_number', $invoice_number )
 			->render();
 	}
 	
@@ -39,6 +51,8 @@ class Woocommerce_Invoice_Meta_Box {
 		if ( ! filter_has_var( INPUT_POST, 'woocommerce_twinfield_sync' ) )
 			return;
 		
-		$this->woocommerce_twinfield_sync->sync( $post_id );
+		$customer_id = filter_input( INPUT_POST, 'woocommerce_invoice_customer_id', FILTER_SANITIZE_STRING );
+		
+		$this->woocommerce_invoice_sync->sync( $post_id, $customer_id );
 	}
 }

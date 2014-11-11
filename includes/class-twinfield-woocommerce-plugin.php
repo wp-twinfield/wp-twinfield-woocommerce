451dc26d4f93b8331a4cad8f5454a687b19ceb67
Remco Tolsma <info@remcotolsma.nl>
2014-11-11 12:03:21 +0100

    Simplified the plugin class.

--- includes/class-twinfield-woocommerce-plugin.php
+++ includes/class-twinfield-woocommerce-plugin.php
@@ -1,22 +1,18 @@
 <?php
 
 /**
- * Plugin class that adds support for twinfield_article to the product
- * post type and sets the hook for wp_twinfield_formbuilder_load_forms
- * to load the Woocommerce Invoice Form for the FormBuilder UI
- *
- * @package Woocommerce Twinfield
- *
- * @author Leon Rowland <leon@rowland.nl>
+ * Title: Twinfield WooCommerce plugin
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2014
+ * Company: Pronamic
+ * @author Remco Tolsma
  * @version 1.0.0
  */
 class Pronamic_Twinfield_WooCommerce_Plugin {
-
 	/**
-	 * Sets the product post type to support the twinfield_article
-	 * metabox.
+	 * Constructs and initialize plugin
 	 *
-	 * @hooks ACTION wp_twinfield_load_forms
+	 * @param unknown $file
 	 */
 	public function __construct( $file ) {
 		$this->file = $file;
@@ -24,42 +20,25 @@ class Pronamic_Twinfield_WooCommerce_Plugin {
 		add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ) );
 	}
 
+	//////////////////////////////////////////////////
+
+	/**
+	 * Plugins loaded
+	 */
 	public function plugins_loaded() {
 		/*
 		 * WooCommerce version >= 2.1.0 = WC_VERSION          » https://github.com/woothemes/woocommerce/blob/v2.1.0/woocommerce.php#L255-L256
 		 * WooCommerce version <  2.1.0 = WOOCOMMERCE_VERSION » https://github.com/woothemes/woocommerce/blob/v2.0.20/woocommerce.php#L132-L133
 		 */
 		if ( defined( 'WOOCOMMERCE_VERSION' ) ) {
-			$this->register_hooks();
-
+			// Text domain
 			load_plugin_textdomain( 'twinfield_woocommerce', false, dirname( plugin_basename( $this->file ) ) . '/languages/' );
+
+			// Post types
+			add_post_type_support( 'product', 'twinfield_article' );
+			add_post_type_support( 'shop_order', 'twinfield_invoiceable' );
+
+			\Pronamic\WP\Twinfield\Invoice\InvoiceMetaBoxFactory::register( 'shop_order', 'Pronamic_Twinfield_WooCommerce_InvoiceMetaBox' );
 		}
-	}
-
-	/**
-	 * Called if the WooCommerce class exists to ensure that the plugin dependancy
-	 * is met.
-	 *
-	 * As long as there are not hooks before 'plugins_loaded' action, no problems will
-	 * occur.
-	 *
-	 * The actions before 'plugins_loaded' are as follows: mu_plugins_loaded, registered_taxonomy and
-	 * registered_post_type.
-	 *
-	 * @see http://codex.wordpress.org/Plugin_API/Action_Reference#Actions_Run_During_an_Admin_Page_Request
-	 *
-	 * @access public
-	 * @return void
-	 */
-	public function register_hooks() {
-		// Add the Twinfield Article Metabox to the Product Post Type
-		add_post_type_support( 'product', 'twinfield_article' );
-		add_post_type_support( 'shop_order', 'twinfield_invoiceable' );
-
-		add_action( 'admin_init', array( $this, 'admin_init' ) );
-	}
-
-	public function admin_init() {
-		\Pronamic\WP\Twinfield\Invoice\InvoiceMetaBoxFactory::register( 'shop_order', 'Pronamic_Twinfield_WooCommerce_InvoiceMetaBox' );
 	}
 }

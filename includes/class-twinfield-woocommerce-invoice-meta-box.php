b5709639526d73294041b3e743d8dbb2de9d16e3
Remco Tolsma <info@remcotolsma.nl>
2014-11-11 15:14:02 +0100

    Fixed an warning.

--- includes/class-twinfield-woocommerce-invoice-meta-box.php
+++ includes/class-twinfield-woocommerce-invoice-meta-box.php
@@ -1,7 +1,7 @@
 <?php
 
 class Pronamic_Twinfield_WooCommerce_InvoiceMetaBox {
-	public function prepare_sync( $post_id, $customer_id, $invoice_id = null, $invoice_type = null ) {
+	public function prepare_sync( $post_id ) {
 		$wc_order = new WC_Order( $post_id );
 
 		return new Pronamic_Twinfield_WooCommerce_Invoice( $wc_order );

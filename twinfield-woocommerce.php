1ba787009d2fa8f10e3bc12e72b0210fb7224453
Remco Tolsma <info@remcotolsma.nl>
2014-11-11 11:31:53 +0100

    Use the 'twinfield_bootstrap' action to make sure that the Twinfield plugin is loaded.

--- twinfield-woocommerce.php
+++ twinfield-woocommerce.php
@@ -17,15 +17,19 @@ License: GPL
 GitHub URI: https://github.com/wp-twinfield/wp-twinfield-woocommerce
 */
 
-if ( defined( 'PRONAMIC_TWINFIELD_FILE' ) ) {
+function twinfield_woocommerce_bootstrap() {
+	$path = plugin_dir_path( __FILE__ );
+
 	// Requirements
-	require_once 'includes/class-twinfield-woocommerce-invoice.php';
-	require_once 'includes/class-twinfield-woocommerce-invoice-meta-box.php';
-	require_once 'includes/class-twinfield-woocommerce-integration.php';
-	require_once 'includes/class-twinfield-woocommerce-plugin.php';
+	require_once $path . 'includes/class-twinfield-woocommerce-invoice.php';
+	require_once $path . 'includes/class-twinfield-woocommerce-invoice-meta-box.php';
+	require_once $path . 'includes/class-twinfield-woocommerce-integration.php';
+	require_once $path . 'includes/class-twinfield-woocommerce-plugin.php';
 
 	// Loads the plugin class into global state.
 	global $twinfield_woocommerce;
 
 	$twinfield_woocommerce = new Pronamic_Twinfield_WooCommerce_Plugin( __FILE__ );
 }
+
+add_action( 'twinfield_bootstrap', 'twinfield_woocommerce_bootstrap' );
